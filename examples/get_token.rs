4ac852ec637b7a7d02f46f824668134bac8eee04
zjhsdtc <zjhsdtc@gmail.com>
2020-08-19 03:36:04 +0800

    chore: use online testnet for spec and demo

--- examples/get_token.rs
+++ examples/get_token.rs
@@ -1,7 +1,11 @@
 use okchain_sdk_lib::*;
 
 pub fn main() {
-    let rpc_client = OKChainRpcClient::new("http://127.0.0.1:26657");
-    let result = rpc_client.get_token("okb");
+    // let rpc_client = OKChainRpcClient::new("http://127.0.0.1:26657");
+    let rpc_client = OKChainRpcClient::new("tcp://35.176.62.211:26657");
+    let result = rpc_client.get_tokens();
+    println!("{:?}", result);
+
+    let result = rpc_client.get_token("tokt");
     println!("{:?}", result);
 }
